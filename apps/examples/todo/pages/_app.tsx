ebf822e6c818a375157d6c2571dd6d93e62918b3
Alexander Blöndal <alexander@slate.se>
2023-04-27 16:42:43 +0000

    Small test

--- apps/examples/todo/pages/_app.tsx
+++ apps/examples/todo/pages/_app.tsx
@@ -6,22 +6,50 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import { RealtimeProvider } from '@/realtime.config'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import { useAutosave } from '@inrealtime/react'
 
 dayjs.extend(utc)
 
 export default function App({ Component, pageProps }: AppProps) {
-  const startOfHour = useMemo(() => dayjs().utc().format('YYMMDDHH').toString(), [])
+  const [startOfHour, setStartOfHour] = useState('123')
+  //const startOfHour = useMemo(() => dayjs().utc().format('YYMMDDHH').toString(), [])
+  const { createNewDocument } = useAutosave({ storeNamePostfix: 'test', disableWarning: true })
+
   return (
     <>
       <Head>
         <title>Realtime Example | Todo</title>
       </Head>
-
+      <button
+        onClick={() => {
+          const id = `${Math.floor(Math.random() * 5000) + 10000}`
+          createNewDocument({
+            documentId: id,
+            document: {
+              todos: [
+                {
+                  id: '1',
+                  label: 'test',
+                  isCompleted: false,
+                },
+                {
+                  id: '2',
+                  label: 'test2',
+                  isCompleted: true,
+                },
+              ],
+            },
+          }).then(() => [setStartOfHour(id)])
+        }}
+      >
+        Test
+      </button>
       <RealtimeProvider
         documentId={startOfHour}
         publicAuthKey={process.env.NEXT_PUBLIC_REALTIME_PUBLIC_AUTH_KEY}
         throttle={20}
+        autosave={{ storeNamePostfix: 'test', disableWarning: true }}
       >
         <Component {...pageProps} />
       </RealtimeProvider>
