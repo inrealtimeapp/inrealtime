82f23098fc6702f695bfb9d8c3667869f3bab808
Alexander Blöndal <alexander@slate.se>
2023-04-27 15:02:48 +0000

    Small fixes

--- apps/examples/todo/pages/_app.tsx
+++ apps/examples/todo/pages/_app.tsx
@@ -6,15 +6,12 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import { RealtimeProvider } from '@/realtime.config'
-import { useMemo, useState } from 'react'
-import { useAutosave } from '@inrealtime/react'
+import { useMemo } from 'react'
 
 dayjs.extend(utc)
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [startOfHour, setStartOfHour] = useState('3294')
-  //const startOfHour = '3294' //useMemo(() => dayjs().utc().format('YYMMDDHH').toString(), [])
-  const { createNewDocument } = useAutosave({ disableWarning: true, storeNamePostfix: 'test' })
+  const startOfHour = useMemo(() => dayjs().utc().format('YYMMDDHH').toString(), [])
   return (
     <>
       <Head>
@@ -24,40 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
         documentId={startOfHour}
         publicAuthKey={process.env.NEXT_PUBLIC_REALTIME_PUBLIC_AUTH_KEY}
         throttle={20}
-        autosave={{ disableWarning: true, storeNamePostfix: 'test' }}
-        _package={{
-          environment: 'development',
-          logging: {
-            socketStatus: true,
-          },
-        }}
       >
-        <button
-          onClick={() => {
-            const id = Math.floor(Math.random() * 5000 + 1)
-            createNewDocument({
-              documentId: `${id}`,
-              document: {
-                todos: [
-                  {
-                    id: '1',
-                    label: 'Test',
-                    isCompleted: false,
-                  },
-                ],
-              },
-            })
-              .catch((err) => {
-                console.log(err)
-              })
-              .then(() => {
-                console.log('Created ids ', id)
-                setStartOfHour(`${id}`)
-              })
-          }}
-        >
-          Test
-        </button>
         <Component {...pageProps} />
       </RealtimeProvider>
     </>
